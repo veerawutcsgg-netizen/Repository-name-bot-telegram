ea6ca7658c96618269e185c2ea5ff78fad3f70d7
veerawutcsgg-netizen <veerawutcsgg@gmail.com>
2026-04-15 22:34:17 +0700

    bot.py

--- Bot/bot.py
+++ Bot/bot.py
@@ -195,4 +195,5 @@ def logout():
     session.clear()
     return redirect("/login")
 
-app.run(host="0.0.0.0", port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
